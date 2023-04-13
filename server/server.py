66c4375502ea45d34119ccfa9c537030516b9695
Rbbingham <rbbingham1893@gmail.com>
2023-04-13 12:07:27 -0500

    changes

--- server/server.py
+++ server/server.py
@@ -37,7 +37,7 @@ class Server:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind(("127.0.0.1", self.__port))
 
-        with open(self.__log_location, "w") as logfile, open("recv_file", "wb") as w:
+        with open(self.__log_location, "w") as logfile:
             try:
                 while True:
                     # three-way handshake
